1676bbc485e3365596382988b63d804524152fca
25198 <WENBIN.QIU@HAND-CHINA.COM>
2020-01-07 21:11:23 +0800

    update 2nd  edition

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
         "node":true,    // 支持node
     },
     "extends": "eslint:recommended",    // 继承推荐的规则
+    "parser":"babel-eslint",
     "parserOptions": {  // 额外配置变量
         "ecmaVersion": 6,
         "sourceType": "script"
